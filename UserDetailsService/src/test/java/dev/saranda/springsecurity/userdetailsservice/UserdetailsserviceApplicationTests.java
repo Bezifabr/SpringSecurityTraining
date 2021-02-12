54cce16dcdbc948ca787464ebae42f9989228585
Bezifabr <bezifabr@gmail.com>
2021-02-12 16:25:45 +0100

    Initial commit

---
+++ UserDetailsService/src/test/java/dev/saranda/springsecurity/userdetailsservice/UserdetailsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.saranda.springsecurity.userdetailsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdetailsserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
