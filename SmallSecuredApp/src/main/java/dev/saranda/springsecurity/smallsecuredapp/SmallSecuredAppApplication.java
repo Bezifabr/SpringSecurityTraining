7fa796b3c74921b61fef82a6216b46e42fd7dd1b
Bezifabr <bezifabr@gmail.com>
2021-02-17 01:01:55 +0100

    Initialize Small Secured Application project

---
+++ SmallSecuredApp/src/main/java/dev/saranda/springsecurity/smallsecuredapp/SmallSecuredAppApplication.java
@@ -0,0 +1,13 @@
+package dev.saranda.springsecurity.smallsecuredapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallSecuredAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallSecuredAppApplication.class, args);
+    }
+
+}
---
+++ SmallSecuredApp/src/test/java/dev/saranda/springsecurity/smallsecuredapp/SmallSecuredAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.saranda.springsecurity.smallsecuredapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallSecuredAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
